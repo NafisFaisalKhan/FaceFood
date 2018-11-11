ea96a9e18e82774621190d3a8b7b896dc00c946d
Mehmet Mustafa Musabeyli <musabeyli@wisc.edu>
2018-11-11 03:30:02 -0600

    edit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+ import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // imageData is a base64 encoded string
+     public htmlImageFromCamera: string;
 
-  }
+     constructor(public navCtrl: NavController,
+       //public toastCtrl: ToastController,
+       private camera: Camera,
+       private DomSanitizer: DomSanitizer
+
+     ) {
+
+     }
+
+     ngOnInit() {
+     }
+
+     takePicture(){
+       console.log('take picture');
+       let options = {
+         quality: 100,
+         destinationType: this.camera.DestinationType.DATA_URL,
+         encodingType: this.camera.EncodingType.JPEG,
+         mediaType: this.camera.MediaType.PICTURE
+         //saveToPhotoAlbum: false
+
+       };
+
+       this.camera.getPicture(options)
+        .then((imageData)=>{
+          console.log('end take picture');
+           this.htmlImageFromCamera = "data:image/jpeg;base64," + imageData;
+         })
+         .catch(err=>{
+           console.log(err);
+           alert(err);
+         })
+     }
 
 }
